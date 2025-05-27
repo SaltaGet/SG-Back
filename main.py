d5f3e8dcf56e2e2b23d3a0aa5d65cff4a68fe389
DanielChachagua <danielmchachagua@gmail.com>
2025-05-26 23:46:09 -0300

    port

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
 import uvicorn
 
 if __name__ == "__main__":
-    uvicorn.run("src:app", host="localhost", port=8000, log_level="debug", reload=True)
+    uvicorn.run("src:app", host="localhost", port=9000, log_level="debug", reload=True)
